2cec8cf0d9785069ea0d0b90563a62e213dad970
Nicola Worthington <nicolaw@tfb.net>
2007-01-30 22:10:16 +0000

    git-svn-id: file:///var/tmp/data/svnroot/WWW-FleXtel@927 610ec7d1-1c12-4784-ad90-13d12571cf29

--- lib/WWW/FleXtel.pm
+++ lib/WWW/FleXtel.pm
@@ -44,16 +44,35 @@ sub new {
 	ref(my $class = shift) && croak 'Class name required';
 	croak 'Odd number of elements passed when even was expected' if @_ % 2;
 
+	# Conjure up an invisible object 
 	my $self = bless \(my $dummy), $class;
 	$objstore->{refaddr($self)} = {@_};
 	my $stor = $objstore->{refaddr($self)};
 
+	# Define what parameters are valid for this constructor
 	$stor->{validkeys} = [qw(password account pin number)];
 	my $validkeys = join('|',@{$stor->{validkeys}});
+
+	# Only accept sensible known parameters from punters
 	my @invalidkeys = grep(!/^$validkeys$/,grep($_ ne 'validkeys',keys %{$stor}));
 	delete $stor->{$_} for @invalidkeys;
 	cluck('Unrecognised parameters passed: '.join(', ',@invalidkeys))
 		if @invalidkeys && $^W;
+
+	# Set some default values
+	$stor->{timeout} ||= 15; # 15 seconds
+	$stor->{cache_ttl} ||= 10; # Cache data for 10 seconds
+	$stor->{'user-agent'} ||= sprintf('Mozilla/5.0 (X11; U; Linux i686; '.
+				'en-US; rv:1.8.1.1) Gecko/20060601 Firefox/2.0.0.1 (%s %s)',
+				__PACKAGE__, $VERSION);
+
+	# Create LWP object
+	my $ua = new LWP::UserAgent;
+	$ua->env_proxy;
+	$ua->agent($stor->{'user-agent'});
+	$ua->timeout($stor->{timeout});
+	$ua->max_size(1024 * 200); # Hard code at 200KB
+	$stor->{ua} = $ua;
 
 	DUMP('$self',$self);
 	DUMP('$stor',$stor);
@@ -61,9 +80,9 @@ sub new {
 }
 
 
-sub set_destination { &_executeQuery->{destination}; }
-sub get_destination { &_executeQuery->{destination}; }
-sub get_phonebook { &_executeQuery->{phonebook}; }
+sub set_destination { &_executeQuery; }
+sub get_destination { &_executeQuery; }
+sub get_phonebook   { &_executeQuery; }
 
 
 
@@ -72,13 +91,193 @@ sub get_phonebook { &_executeQuery->{phonebook}; }
 # Private methods
 #
 
-sub _getQueriesData {
-	my %queries = (
-		'&set_destination' => 'reroute',
-		'&get_destination' => 'destination',
-		'&get_phonebook' => 'destination',
+sub _deepCopy{
+	my $this = shift;
+	if (!ref($this)) {
+		$this;
+	} elsif (ref($this) eq 'ARRAY') {
+		[ map _deepCopy($_), @{$this} ];
+	} elsif (ref($this) eq 'HASH'){
+		scalar { map { $_ => _deepCopy($this->{$_}) } keys %{$this} };
+	} else {
+		confess "What type is $_?";
+	}
+}
 
-		'destination' => {
+
+sub _executeQuery {
+	my $self = shift;
+	local $Carp::CarpLevel = 1;
+	croak 'Not called as a method by parent object'
+		unless ref $self && UNIVERSAL::isa($self, __PACKAGE__);
+
+	# Retrieve our object data stor and merge
+	# parameters from this method and the constructor
+	my $stor = $objstore->{refaddr($self)};
+	my %params = @_;
+	for my $k (@{$stor->{validkeys}}) {
+		$params{$k} = $stor->{$k}
+			unless defined $params{$k};
+	}
+
+	# Figure out what method and personality we're running as
+	(my $subr = (caller(1))[3]) =~ s/.*:://;
+	my ($readCache,$cacheName) = split(/_/,$subr);
+	$readCache = 0 unless $readCache eq 'get';
+
+	# Return from a cache if possible
+	if ($readCache) {
+		my $cache = $self->_readCache("$cacheName*$params{number}");
+		if (defined $cache) {
+			TRACE("Returning cache '$cacheName*$params{number}' ...");
+			return $cache;
+		}
+	}
+
+	# Get the post query data required to send to the server
+	my ($mode,$query) = _getQueryData($subr);
+	TRACE("Running _executeQuery() in $mode mode ...");
+	DUMP('$query',$query);
+
+	# Substitute keywords for data values
+	while (my ($k,$v) = each %{$query->{data}}) {
+		$query->{data}->{$k} =~ s/\@\@(\S+)\@\@/$params{$1}/g;
+	}
+
+	# Post the request to the server
+	my $ua = $stor->{ua};
+	$ua->default_header('Referer' => $query->{referer});
+	my $response;
+	if ($query->{method} eq 'GET') {
+		my $url = join('?',$query->{url},
+					join('&',
+						map { "$_=$query->{data}->{$_}" }
+							keys %{$query->{data}}
+					));
+		TRACE($url);
+		$response = $ua->get($url);
+	} else { # Default to POST
+		$response = $ua->post($query->{url}, $query->{data});
+	};
+
+	# Process and parse the HTML response if the request was successfull
+	if ($response->is_success) {
+		my $data = _extractData($response->content);
+		$self->_writeCache("$cacheName*$params{number}", $data->{$cacheName});
+		return $data->{$cacheName};
+
+	# Otherwise croak and die horribly
+	} else {
+		croak $response->status_line;
+	}
+}
+
+
+sub _readCache {
+	my ($self,$cacheName) = @_;
+	my $stor = $objstore->{refaddr($self)};
+
+	if (defined $stor->{cache}->{$cacheName}->{'last_updated'} &&
+			time - $stor->{cache}->{$cacheName}->{'last_updated'}
+			< $stor->{'cache_ttl'}) {
+		return $stor->{cache}->{$cacheName}->{'data'};
+	}
+	return;
+}
+
+
+sub _writeCache {
+	my ($self,$cacheName,$ref) = @_;
+	my $stor = $objstore->{refaddr($self)};
+
+	$stor->{cache}->{$cacheName}->{'last_updated'} = time;
+	$stor->{cache}->{$cacheName}->{'data'} = $ref;
+}
+
+
+sub _phonebookLookup {
+	my ($phonebook, $lookup) = @_;
+	$lookup = '' unless defined $lookup;
+	my $memory = { destination => '', title => '', memory => '' };
+	return $memory unless $lookup =~ /\S/;
+
+	for (my $i = 1; $i < @{$phonebook}; $i++) {
+		my $mem = $phonebook->[$i];
+		$mem->{memory} = $i;
+
+		if ($lookup =~ /^\d+$/ && $i == $lookup) {
+			$memory = $mem;
+		} elsif ($lookup =~ /[0-9\#\+]{8,}/ && $lookup eq $mem->{number}) {
+			$memory = $mem;
+		} elsif ($lookup eq $mem->{title}) {
+			$memory = $mem;
+		}
+	}
+
+	return $memory;
+}
+
+
+sub _extractData {
+	my $html = shift;
+	my %data;
+
+	if ($html =~ /^\s*([0-9\#\+]{8,})\s*$/s) {
+		$data{destination} = $1;
+		return \%data;
+	}
+
+	for (split(/[\n\r]/,$html)) {
+		chomp;
+		if (my ($key,$num,$val) = $_ =~
+				/^\s*FN.(email|dest_(?:no|nrb)|mem(\d+)(?:text)?)\s*=\s*(.+?)\s*;\s*$/) {
+			$val =~ s/^\s*"\s*//g;
+			$val =~ s/\s*"\s*$//g;
+
+			if ($key =~ /^mem\d+$/) {
+				$val =~ s/[^0-9\#]//g;
+				$data{phonebook}->[$num]->{number} = $val;
+
+			} elsif ($key =~ /^mem\d+text$/) {
+				$data{phonebook}->[$num]->{title} = $val;
+
+			} elsif ($key eq 'email' && $val =~ /"(\S+?)"/) {
+				$data{email} = $1;
+
+			} elsif ($key =~ /^dest_no$/) {
+				($data{destination}) = $val =~ /([0-9\#\+]{8,})/;
+				$data{destination} =~ s/[^0-9\#]//g;
+			}
+		}
+	}
+
+	return \%data;
+}
+
+
+sub _getQueryData {
+	my $subr = shift;
+
+	my %subrMap = (
+			'set_destination' => 'divert_simple',
+			'get_destination' => 'getpin_simple',
+			'get_phonebook'   => 'getpin_post',
+		);
+
+	my %queries = (
+		'getpin_simple' => {
+			'method' => 'GET',
+			'url' => 'https://www.flextel.ltd.uk/cgi-bin/reroute.sh',
+			'referer' => '',
+			'data' => {
+				'mode'    => 'getpin',
+				'flextel' => '@@number@@',
+				'pin'     => '@@pin@@',
+				'alt'     => 'simple',
+			},
+		},
+		'getpin_post' => {
+			'method' => 'POST',
 			'url' => 'https://www.flextel.ltd.uk/cgi-bin/reroute.sh',
 			'referer' => 'https://www.flextel.ltd.uk/cgi-bin/reroute.sh?flextel=',
 			'data' => {
@@ -91,8 +290,21 @@ sub _getQueriesData {
 				'Logon'   => 'Logon',
 			},
 		},
-
-		'reroute' => {
+		'divert_simple' => {
+			'method' => 'GET',
+			'url' => 'https://www.flextel.ltd.uk/cgi-bin/reroute.sh',
+			'referer' => '',
+			'data' => {
+				'mode'     => 'divert',
+				'flextel'  => '@@number@@',
+				'pin'      => '@@pin@@',
+				'new_dest' => '@@destination@@',
+				'dest_nrb' => '',
+				'alt'      => 'simple',
+			},
+		},
+		'divert_post' => {
+			'method' => 'POST',
 			'url' => 'https://www.flextel.ltd.uk/cgi-bin/reroute.sh',
 			'referer' => 'https://www.flextel.ltd.uk/cgi-bin/reroute.sh',
 			'data' => {
@@ -118,126 +330,8 @@ sub _getQueriesData {
 		},
 	);
 
-	return \%queries;
-}
-
-
-sub _deepCopy{
-	my $this = shift;
-	if (!ref($this)) {
-		$this;
-	} elsif (ref($this) eq 'ARRAY') {
-		[ map _deepCopy($_), @{$this} ];
-	} elsif (ref($this) eq 'HASH'){
-		scalar { map { $_ => _deepCopy($this->{$_}) } keys %{$this} };
-	} else {
-		confess "What type is $_?";
-	}
-}
-
-
-sub _executeQuery {
-	my $self = shift;
-	croak 'Not called as a method by parent object'
-		unless ref $self && UNIVERSAL::isa($self, __PACKAGE__);
-
-	my $stor = $objstore->{refaddr($self)};
-	my %postDataPairs = @_;
-	for my $k (@{$stor->{validkeys}}) {
-		$postDataPairs{$k} = $stor->{$k}
-			unless defined $postDataPairs{$k};
-	}
-
-	(my $subr = (caller(1))[3]) =~ s/.*:://;
-	my $queries = _getQueriesData();
-	my $mode = $queries->{"&${subr}"};
-	my $query = _deepCopy($queries->{$mode});
-
-	while (my ($k,$v) = each %{$query->{data}}) {
-		$query->{data}->{$k} =~ s/\@\@(\S+)\@\@/$postDataPairs{$1}/g;
-	}
-
-	my $ua = new LWP::UserAgent;
-	$ua->env_proxy;
-	$ua->agent('Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20060601 Firefox/2.0.0.1');
-	$ua->default_header('Referer' => $query->{referer});
-	$ua->timeout(10);
-
-	my $response = $ua->post($query->{url}, $query->{data});
-	if ($response->is_success) {
-		my $data = _extractData($response->content);
-		my $memory = _memoryLookup($data,$data->{destination});
-
-		my $destination = '';
-		$destination .= "$data->{destination}" if $data->{destination};
-		$destination .= " ($memory->{title})" if $memory->{title};
-
-		return {
-				#html => $response->content,
-				data => $data,
-				memory => $memory,
-				phonebook => $data->{memory},
-				destination => $destination,
-			}
-
-	} else {
-		die $response->status_line;
-	}
-}
-
-
-sub _memoryLookup {
-	my ($data, $lookup) = @_;
-	$lookup = '' unless defined $lookup;
-	my $memory = { destination => '', title => '', memory => '' };
-	return $memory unless $lookup =~ /\S/;
-
-	for (my $i = 1; $i < @{$data->{memory}}; $i++) {
-		my $mem = $data->{memory}->[$i];
-		$mem->{memory} = $i;
-
-		if ($lookup =~ /^\d+$/ && $i == $lookup) {
-			$memory = $mem;
-		} elsif ($lookup =~ /\d{8,}/ && $lookup eq $mem->{number}) {
-			$memory = $mem;
-		} elsif ($lookup eq $mem->{title}) {
-			$memory = $mem;
-		}
-	}
-
-	return $memory;
-}
-
-
-sub _extractData {
-	my $html = shift;
-	my %data = (destination => '', email => '');
-
-	for (split(/[\n\r]/,$html)) {
-		chomp;
-		if (my ($key,$num,$val) = $_ =~
-				/^\s*FN.(email|dest_(?:no|nrb)|mem(\d+)(?:text)?)\s*=\s*(.+?)\s*;\s*$/) {
-			$val =~ s/^\s*"\s*//g;
-			$val =~ s/\s*"\s*$//g;
-
-			if ($key =~ /^mem\d+$/) {
-				$val =~ s/[^0-9]//g;
-				$data{memory}->[$num]->{number} = $val;
-
-			} elsif ($key =~ /^mem\d+text$/) {
-				$data{memory}->[$num]->{title} = $val;
-
-			} elsif ($key eq 'email' && $val =~ /"(\S+?)"/) {
-				$data{email} = $1;
-
-			} elsif ($key =~ /^dest_no$/) {
-				($data{destination}) = $val =~ /([0-9\+\- ]{8,})/;
-				$data{destination} =~ s/[^0-9]//g;
-			}
-		}
-	}
-
-	return \%data;
+	my $mode = $subrMap{$subr};
+	return ($mode, _deepCopy($queries{$mode}));
 }
 
 
@@ -282,14 +376,14 @@ WWW::FleXtel - Manipulate FleXtel phone number redirection
  use Data::Dumper qw(Dumper);
  
  my $flextel = WWW::FleXtel->new(
+         number   => "0701776655",
+         pin      => "1234",
          account  => "A99999",
-         number   => "0701773355",
-         pin      => "21234",
          password => "password",
      );
  
  printf("Diverted to %s\n", $flextel->get_destination);
- $flextel->set_destination(destination => "0800883322");
+ $flextel->set_destination(destination => "01923001122");
  printf("Diverted to %s\n", $flextel->get_destination);
  
  print Dumper($flextel->get_phonebook);
@@ -309,9 +403,9 @@ release.
 =head2 new
 
  my $flextel = WWW::FleXtel->new(
+         number   => "0701776655",
+         pin      => "1234",
          account  => "A99999",
-         number   => "0701773355",
-         pin      => "21234",
          password => "password",
      );
 
@@ -322,7 +416,7 @@ release.
 
 =head2 set_destination
 
- my $destination = $flextel->set_destination(destination => "0800883322");
+ my $destination = $flextel->set_destination(destination => "01923001122");
  print "Diverted to $destination\n";
 
 =head2 get_phonebook
@@ -330,6 +424,12 @@ release.
  my $phonebook = $flextel->get_phonebook;
  use Data::Dumper qw(Dumper);
  print Dumper($phonebook);
+
+This method extracts the indexes, names and numbers from your FleXtel
+number's phonebook. This method has the potential to stop working in
+the future since it gathers the information by performing very basic
+parsing of the JavaScript object assignments on your FleXtel number's
+rerouting webpage.
 
 =head1 SEE ALSO
 
