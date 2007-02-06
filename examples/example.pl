e2107a405fbf1953e2d40e2b5e65dec7832bd376
Nicola Worthington <nicolaw@tfb.net>
2007-02-06 16:33:55 +0000

    git-svn-id: file:///var/tmp/data/svnroot/WWW-FleXtel@936 610ec7d1-1c12-4784-ad90-13d12571cf29

--- examples/example.pl
+++ examples/example.pl
@@ -36,10 +36,14 @@ my %acct = (
 
 my $flextel = WWW::FleXtel->new(%acct);
 
-printf("Diverted to %s\n", $flextel->get_destination);
 my $destination = $flextel->set_destination(destination => "0800883322");
-printf("Diverted to %s\n", $destination);
-    
+
+printf("Email: %s\nDestination: %s\nICD: %s\n",
+		$flextel->get_email,
+		$flextel->get_destination,
+		$flextel->get_icd,
+	);
+
 print Dumper($flextel->get_phonebook);
 
 exit;
